5dcd864276e792ee8dc8b4b22e96ad8c98275620
ofthewildfire <lissajace@gmail.com>
2023-11-26 16:56:22 +0200

    added express.json middleware

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const axios = require("axios")
 
 const app = express()
 
+app.use(express.json())
 app.use(cors())
 
 app.get("/shorten", async (req, res) => {
 	try {
 		const response = await axios.post("https://cleanuri.com/api/v1/shorten", {
-			url: "https://google.com",
+			url: req.body.url,
 		})
 		console.log(response.data.result_url)
 		res.json(response.data)
